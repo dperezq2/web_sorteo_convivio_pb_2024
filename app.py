e8de8c12c877923ac3d4ba3d24bef192ee8acbec
Douglas Pérez <dperezq2@miumg.edu.gt>
2024-11-28 17:07:24 -0600

    Cambios de diseño

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def result():
     return render_template('result.html', winner_name=winner_name, winner_photo=winner_photo, total_participants=total_participants)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
